70710941d08b58076e4f9edc3139c97187369e63
Kenet96 <31292746+Kenet96@users.noreply.github.com>
2017-09-27 16:33:20 -0500

    A<RGREGREAERTGRRGRTAG

--- regresion.php
+++ regresion.php
@@ -1,33 +1,4 @@
 <?php
-$xx=$_POST['x'];
-$yy=$_POST['y'];
-	$xxx=explode("\n",$xx); 
-	$yyy=explode("\n",$yy);
-	$totalx = array_sum($xxx);
-	$ny=count($xxx);
-	$promx=$totalx/$ny;
-	$totaly = array_sum($yyy);
-	$nx=count($yyy);
-	$promy=$totaly/$nx;
-	echo "n  = \n".$nx."<br>";
-	echo "Promedio de x = \n".$promx."<br>";
-	echo "Promedio de y = \n".$promy."<br>";
-	$sumcx=0;
-	$sumpxy=0;
-	for ($i=0;$i<$nx;$i++)
-	{
-	$sumcx=$sumcx+$xxx[$i]*$xxx[$i];
-	$sumpxy=$sumpxy+$xxx[$i]*$yyy[$i];
-	}
-	$sumacx=$sumcx-$nx*$promx*$promx;
-	$sumapxy=$sumpxy-$nx*$promx*$promy;
-	$b1=$sumapxy/$sumacx;
-	$bo=$promy-$b1*$promx;
-	
-	echo "La ecuacion de regresion es  :"modificando en ads "<br>";
-	echo "Y=".$bo." + ".$b1." x"."<br>";
-	
-?>
 </head>
 <body>
 Digite los valores X e Y:
